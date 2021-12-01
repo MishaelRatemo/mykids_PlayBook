de9f4cbacaa5bbb273ba70c5b23511199af24d1f
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2021-12-01 18:48:06 +0300

    added functionality for number to make sound when clicked

--- js/math.js
+++ js/math.js
@@ -10,3 +10,31 @@ function three(){
   var my_sound = document.getElementById('3');
   my_sound.play();
 }
+function four(){
+  var my_sound = document.getElementById('4');
+  my_sound.play();
+}
+function five(){
+  var my_sound = document.getElementById('5');
+  my_sound.play();
+}
+function six(){
+  var my_sound = document.getElementById('6');
+  my_sound.play();
+}
+function seven(){
+  var my_sound = document.getElementById('7');
+  my_sound.play();
+}
+function eight(){
+  var my_sound = document.getElementById('8');
+  my_sound.play();
+}
+function nine(){
+  var my_sound = document.getElementById('9');
+  my_sound.play();
+}
+function ten(){
+  var my_sound = document.getElementById('10');
+  my_sound.play();
+}
