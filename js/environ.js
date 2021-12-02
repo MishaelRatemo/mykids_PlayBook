895579a265fde07b069ee79c829cff10cb9e4b09
faisoabdirisak <faisoabdirisak@gmail.com>
2021-12-02 10:41:21 +0300

    it will add sounds on environment

--- js/environ.js
+++ js/environ.js
@@ -38,3 +38,78 @@ function play() {
     var audio = document.getElementById("audio7");
     audio.play();
   }
+  // sun
+  function playsun() {
+    var audio = document.getElementById("audiosun");
+    audio.play();
+  }
+  //moon
+  function playmoon() {
+    var audio = document.getElementById("audiomoon");
+    audio.play();
+  }
+  //stars
+  function playstars() {
+    var audio = document.getElementById("audiostars");
+    audio.play();
+  }
+  //sky
+  function playsky() {
+    var audio = document.getElementById("audiosky");
+    audio.play();
+  }
+  //leave
+  function playleave() {
+    var audio = document.getElementById("audioleave");
+    audio.play();
+  }
+  //trees
+  function playtree() {
+    var audio = document.getElementById("audiotree");
+    audio.play();
+  }
+  //forest
+  function playforest() {
+    var audio = document.getElementById("audioforest");
+    audio.play();
+  }
+  //grass
+  function playgrass() {
+    var audio = document.getElementById("audiograss");
+    audio.play();
+  }
+  //ocean
+  function playocean() {
+    var audio = document.getElementById("audiocean");
+    audio.play();
+  }
+  //river
+  function playriver() {
+    var audio = document.getElementById("audioriver");
+    audio.play();
+  }
+  //lake
+  function playlake() {
+    var audio = document.getElementById("audiolake");
+    audio.play();
+  }
+  //waterfall
+  function playwaterfall() {
+    var audio = document.getElementById("audiowaterfall");
+    audio.play();
+  }
+  //mountain
+  function playmount() {
+    var audio = document.getElementById("audiomount");
+    audio.play();
+  }
+  //hill
+  function playhill() {
+    var audio = document.getElementById("audiohill");
+    audio.play();
+  }
+  //mountains
+  function playmaunt() {
+    var audio = document.getElementById("audiomaunt");
+    audio.play();
+  }
