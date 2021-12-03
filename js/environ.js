23fbae86b26408d9d247b4a7ebe4c84455b04c83
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2021-12-03 10:38:23 +0300

    styled envirinment page and its functionality

---
+++ js/environ.js
@@ -0,0 +1,115 @@
+// red
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
+//   blue
+  function play1() {
+    var audio = document.getElementById("audio1");
+    audio.play();
+  }
+//   yellow
+  function play2() {
+    var audio = document.getElementById("audio2");
+    audio.play();
+  }
+//   green
+  function play3() {
+    var audio = document.getElementById("audio3");
+    audio.play();
+  }
+//   orange
+  function play4() {
+    var audio = document.getElementById("audio4");
+    audio.play();
+  }
+//   black
+  function play5() {
+    var audio = document.getElementById("audio5");
+    audio.play();
+  }
+//   pink
+  function play6() {
+    var audio = document.getElementById("audio6");
+    audio.play();
+  }
+//   purple
+  function play7() {
+    var audio = document.getElementById("audio7");
+    audio.play();
+  }
+  // sun
+  function playsun() {
+    var audio = document.getElementById("audiosun");
+    audio.play();
+  }
+  //moon
+  function playmoon() {
+    var audio = document.getElementById("audiomoon");
+    audio.play();
+  }
+  //stars
+  function playstars() {
+    var audio = document.getElementById("audiostars");
+    audio.play();
+  }
+  //sky
+  function playsky() {
+    var audio = document.getElementById("audiosky");
+    audio.play();
+  }
+  //leave
+  function playleave() {
+    var audio = document.getElementById("audioleave");
+    audio.play();
+  }
+  //trees
+  function playtree() {
+    var audio = document.getElementById("audiotree");
+    audio.play();
+  }
+  //forest
+  function playforest() {
+    var audio = document.getElementById("audioforest");
+    audio.play();
+  }
+  //grass
+  function playgrass() {
+    var audio = document.getElementById("audiograss");
+    audio.play();
+  }
+  //ocean
+  function playocean() {
+    var audio = document.getElementById("audiocean");
+    audio.play();
+  }
+  //river
+  function playriver() {
+    var audio = document.getElementById("audioriver");
+    audio.play();
+  }
+  //lake
+  function playlake() {
+    var audio = document.getElementById("audiolake");
+    audio.play();
+  }
+  //waterfall
+  function playwaterfall() {
+    var audio = document.getElementById("audiowaterfall");
+    audio.play();
+  }
+  //mountain
+  function playmount() {
+    var audio = document.getElementById("audiomount");
+    audio.play();
+  }
+  //hill
+  function playhill() {
+    var audio = document.getElementById("audiohill");
+    audio.play();
+  }
+  //mountains
+  function playmaunt() {
+    var audio = document.getElementById("audiomaunt");
+    audio.play();
+  }
